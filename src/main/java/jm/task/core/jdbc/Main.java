f9e3958062467cd939f5558847305f89f9a12b2a
Dmitriy Gubankov <dmitriy@gubankov.ru>
2021-03-01 11:12:33 +0300

    Realized

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Ivan", "Ivanov", (byte)10);
+        userService.saveUser("Vasiliy", "Vasiliev", (byte)15);
+        userService.saveUser("Petr", "Petrov", (byte)30);
+        userService.saveUser("Vasya", "Pupkin", (byte)100);
+        userService.getAllUsers().stream().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static Connection getConnection(String hostName, String dbName,
+                                           String userName, String password) throws SQLException,
+            ClassNotFoundException{
+        Class.forName("com.mysql.jdbc.Driver");
+
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+
+        return DriverManager.getConnection(connectionURL, userName,
+                password);
+    }
 }
